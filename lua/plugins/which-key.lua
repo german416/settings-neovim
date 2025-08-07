65560c28a7bc26e37886931b57e9a43a1383b254
German Rodrigo Alvarez Fabre <geralvarez.085@gmail.com>
2025-08-06 22:47:11 -0300

    style(which-key): set classic style with single border

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,26 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- Choose one preset:
+    preset = "classic",
+    -- preset = "none",
+    -- preset = "modern",
+    -- preset = "helix",
+
+    -- Choose border:
+    win = {
+      border = "single"
+      -- border = "double"
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+}
