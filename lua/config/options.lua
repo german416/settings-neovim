67aaf0c318f17fd7b77cd2fd68dcb5f6e11355ef
German Rodrigo Alvarez Fabre <geralvarez.085@gmail.com>
2025-08-19 21:29:11 -0300

    feat(options): add support for system clipboard on X11

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- This only works on X11
+vim.g.clipboard = {
+  name = "xclip",
+  copy = {
+    ["+"] = "xclip -selection clipboard -in",
+    ["*"] = "xclip -selection primary -in",
+  },
+  paste = {
+    ["+"] = "xclip -selection clipboard -out",
+    ["*"] = "xclip -selection primary -out",
+  },
+  cache_enabled = true,
+}
+
+-- Enable system clipboard
+vim.opt.clipboard = "unnamedplus"
