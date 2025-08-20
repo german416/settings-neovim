654a237474fb4d90ef090f48cebe5f4806d1c091
German Rodrigo Alvarez Fabre <geralvarez.085@gmail.com>
2025-08-19 21:37:13 -0300

    feat(keymaps): add shortcuts to open Oil (normal and floating mode)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "<leader>o", "<cmd>Oil<cr>", { desc = "Open Oil" })
+map("n", "<leader>O", "<cmd>Oil --float<cr>", { desc = "Open Oil in floating mode" })
