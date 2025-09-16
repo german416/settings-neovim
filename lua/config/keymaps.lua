095b9efda8ba4c628202f5583227166405929eae
German Rodrigo Alvarez Fabre <geralvarez.085@gmail.com>
2025-09-15 21:54:42 -0300

    feat(keymap): modify Oil keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,13 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 
-map("n", "<leader>o", "<cmd>Oil<cr>", { desc = "Open Oil" })
-map("n", "<leader>O", "<cmd>Oil --float<cr>", { desc = "Open Oil in floating mode" })
+map("n", "<leader>o", function()
+  require("oil").open(LazyVim.root())
+end, { desc = "Open Oil explorer (root dir)" })
+
+map("n", "<leader>O", function()
+  require("oil").open(vim.fn.getcwd())
+end, { desc = "Open Oil explorer (cwd)" })
+
+-- map("n", "<leader>o", "<cmd>Oil <cr>", { desc = "Open Oil explorer (root dir)" })
+-- map("n", "<leader>O", "<cmd>Oil <cr>", { desc = "Open Oil explorer (cwd)" })
